4aa8aa1ef677b3ff4220fbd96d5262f2f3a81060
banasrini <bana.srini@gmail.com>
2014-06-06 12:50:37 -0700

    Update raspberrypi.py

--- raspberrypi.py
+++ raspberrypi.py
@@ -1,21 +1,14 @@
 from Pubnub import Pubnub
-from IoT_wrapper import IOTWrapper
+from IoT_wrapper import iotwrapper
 
-pubnub = Pubnub( 'demo', 'demo', None, False)
+pi = iotwrapper(publish_key = 'demo', subscribe_key = 'demo', uuid = 'PI')
 
-pi = IOTWrapper(pubnub)
-
-uuid = 'PI';
-print("My UUID is: "+uuid)
-pubnub.uuid = uuid;
 channel = 'button-reply'
 message = "hello from pi"
 
 
 pi.send(channel, message)
-print("Sending message on channel")
 
-print("Listening for messages on channel")
 pi.connect(channel)
 
 
