6b4e019eca0a980e509ac6a4ba5a9b4067ca553f
banasrini <bana.srini@gmail.com>
2014-06-11 14:46:30 -0700

    Update raspberrypi.py

--- raspberrypi.py
+++ raspberrypi.py
@@ -1,15 +1,17 @@
 from Pubnub import Pubnub
 from IoT_wrapper import iotwrapper
 
-pi = iotwrapper(publish_key = 'demo', subscribe_key = 'demo', uuid = 'PI')
+pi = iotwrapper(publish_key = 'demo', subscribe_key = 'demo', uuid = 'PI', receiver ='do_something')
 
 channel = 'button-reply'
 message = "hello from pi"
 
-
+def receiver(message):
+  print(message)
+  
 pi.send(channel, message)
 
-pi.connect(channel)
+pi.connect(channel, receiver)
 
 
   
