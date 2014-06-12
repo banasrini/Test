b007942a0f1d592ad713838cc4e246e43e3f369c
banasrini <bana.srini@gmail.com>
2014-06-12 11:25:10 -0700

    Update raspberrypi.py

--- raspberrypi.py
+++ raspberrypi.py
@@ -6,11 +6,14 @@ pi = iotwrapper(publish_key = 'demo', subscribe_key = 'demo', uuid = 'PI')
 channel = 'button-reply'
 message = "hello from pi"
 
+# callback function for the user to play around with the received message
 def do_something(message):
   print(message)
-  
+
+# Sending messages on the channel
 pi.send(channel, message)
 
+# Receiving messages on the channel
 pi.connect(channel, do_something)
 
 
