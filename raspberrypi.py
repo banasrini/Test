f817630c9e251211e580e20ee28090e3121a200c
banasrini <bana.srini@gmail.com>
2014-06-13 10:53:10 -0700

    Update raspberrypi.py

--- raspberrypi.py
+++ raspberrypi.py
@@ -1,20 +1,20 @@
 from Pubnub import Pubnub
-from IoT_wrapper import iotwrapper
+from IoT_wrapper import iotbridge
 
-pi = iotwrapper(publish_key = 'demo', subscribe_key = 'demo', uuid = 'PI')
+pi = iotbridge(publish_key = 'demo', subscribe_key = 'demo', uuid = 'PI')
 
 channel = 'button-reply'
 message = "hello from pi"
 
 # callback function for the user to play around with the received message
-def do_something(message):
+def callbackfn(message):
   print(message)
 
 # Sending messages on the channel
 pi.send(channel, message)
 
 # Receiving messages on the channel
-pi.connect(channel, do_something)
+pi.connect(channel, callbackfn)
 
 
   
